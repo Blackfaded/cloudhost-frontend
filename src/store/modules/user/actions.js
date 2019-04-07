c556ab070ce3968e826b4eb30b2242fc4529bcc6
Heinen, Rene <rene.heinen@hsrw.org>
2019-04-07 21:57:52 +0200

    fix(cookie): fix bug where jwt did not get removed correctly

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -2,12 +2,14 @@ import * as Cookies from 'js-cookie';
 import { SET_USER, DESTROY_USER } from './mutationTypes'; //eslint-disable-line
 import axios from '@/config/axios';
 
+const domain = process.env.VUE_APP_DOMAIN.split(/^https?:\/\//)[1];
+
 export default {
   setToken(context, data) {
-    Cookies.set('jwt', data, { domain: process.env.VUE_APP_DOMAIN.split(/^https?:\/\//)[1] });
+    Cookies.set('jwt', data, { domain });
   },
   destroyToken() {
-    Cookies.remove('jwt');
+    Cookies.remove('jwt', { domain });
   },
   async setUser({ commit }) {
     try {
